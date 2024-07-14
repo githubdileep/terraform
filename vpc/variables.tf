c50920a19cd634c2d920d8fa7186db106b7ec34f
Dileep <adileepkumar123@gmail.com>
2024-07-14 17:59:08 +0530

    Add variables.tf

---
+++ vpc/variables.tf
@@ -0,0 +1,33 @@
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr_block" {
+  description = "The CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr_block" {
+  description = "The CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.11.0/24"
+}
+
+variable "database_subnet_cidr_block" {
+  description = "The CIDR block for the database subnet"
+  type        = string
+  default     = "10.0.21.0/24"
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resources"
+  type        = map(string)
+  default = {
+    Name        = "project-timing-1"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
